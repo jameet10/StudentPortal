d52f41f5a3680c462a9b7172f1838a3ac146982b
Devansh2426226 <devanshpandey2604@gmail.com>
2025-08-23 00:13:11 +0530

    changes in style font in the front end

--- Portal/book/book_library/models.py
+++ Portal/book/book_library/models.py
@@ -6,6 +6,8 @@ class enrollmentdetail(models.Model):
     email=models.CharField(max_length=120)
     password=models.CharField(max_length=10)
     date=models.DateField()
+    def __str__(self):
+        return self.name
 
 
   
