1562530e1fe72939e215202c135449564b47203f
jameet10 <jasmeetkaur100606@gmail.com>
2025-08-28 19:19:37 +0530

    Final Changes

--- Portal/book/book_library/urls.py
+++ Portal/book/book_library/urls.py
@@ -12,11 +12,10 @@ urlpatterns = [
     path("library_availibility/", views.library_availibility, name="library_availibility"), 
     path("book_issue", views.book_issue, name="book_issue"),
     path("solved_p", views.solved_p, name="solved_p"),
-<<<<<<< HEAD
+
     path("lib_e", views.save_lib_e, name="lib_e"),
-=======
+
     path("lib_e", views.save_lib_e, name="save_lib_e"),
->>>>>>> 74cad7ea9c98a86a45e7f8819c842cc09b57bcd7
     path('search_pyqs', views.search_pyqs, name='search_pyqs'),
     
 
